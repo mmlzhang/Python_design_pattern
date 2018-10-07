5375b9366cd9ef52520f6b8fcfe99a3360c68a48
zhangminglu <1367000465@qq.com>
2018-10-07 23:58:57 +0800

    添加代理模式代码

---
+++ 09-proxy_pattern/09-2-proxy.py
@@ -0,0 +1,48 @@
+
+class SensitiveInfo(object):
+
+    def __init__(self):
+        self.users = ['nick', 'tom', 'ben', 'mike']
+
+    def read(self):
+        print('There are {} users: {}'.format(len(self.users), " ".join(self.users)))
+
+    def add(self, user):
+        self.users.append(user)
+        print("Added user: ", user)
+
+
+class Info(object):
+    """
+    SensitiveInfo的保护代理
+    """
+    def __init__(self):
+        self.protected = SensitiveInfo()
+        self.secret = '123'
+
+    def read(self):
+        self.protected.read()
+
+    def add(self, user):
+        sec = input("What is the secret?")
+        self.protected.add(user) if sec == self.secret else print("Secret Wrong!")
+
+
+def main():
+    info = Info()
+    while True:
+        print("1 查看用户 \n2 添加用户 \n3 退出")
+        key = input("choose number to option: ")
+        if key == '1':
+            info.read()
+        elif key == '2':
+            name = input("Choose username:")
+            info.add(name)
+        elif key == '3':
+            exit()
+        else:
+            print("Unknow option:", key)
+
+
+if __name__ == '__main__':
+    main()
